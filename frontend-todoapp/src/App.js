480d4712983b1ab5abb630cf8d8b9a6a590b768e
Thiemo Orrico <thiemobile@o2online.de>
2021-10-05 11:59:10 +0200

    Get with filters from fetch implement

--- frontend-todoapp/src/App.js
+++ frontend-todoapp/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-//import TodoItem from "./components/TodoItem";
 import TodoList from "./components/TodoList";
 
 import React, {useState, useEffect} from 'react';
@@ -7,59 +6,67 @@ import React, {useState, useEffect} from 'react';
 
 function App() {
 
-    const urlEndpoint = "localhost:8080/api/todo";
+    const urlEndpoint = "/api/todo";
+
+    const [todoStatus, setTodoStatus] = useState([]);
+    const [inProgressStatus, setInProgressStatus] = useState([]);
+    const [doneStatus, setDoneStatus] = useState([]);
 
 
-
-
-    //const [state, setState] = useState(initialState);
-    //setState(newState);
-
-    const [schnitzel, setSchnitzel] = useState([
-        {
-            "id": "2fbffb09-005d-4165-895c-77fe7f15d924",
-            "description": "Aufgabe1",
-            "status": "todo"
-        },
-        {
-            "id": "4cdc5ec0-21b8-4f2f-9ea9-78b7831f4f1c",
-            "description": "Aufgabe2",
-            "status": "inProgress"
-        },
-        {
-            "id": "ad6bfe7e-c5aa-4354-992a-bf5059172ee9",
-            "description": "Aufgabe3",
-            "status": "todo"
-        }
-
-
-    ]);
-
-    // use effect:
-
-    /*
-    const getTodoListElemtents = (urlEndpoint) => {
-        fetch(urlEndpoint)
+    const getTodoListElemtents = (jsonUrl) => {
+        fetch(jsonUrl)
             .then(response => response.json())
-            .then(data => console.log(data));
+            .then ( responseJson => setTodoStatus(responseJson
+                    .filter(todoStatus => todoStatus.status.includes("todo"))
+                    ))
+            .then(data => console.log("hier die ausgabe von use Effect: " + data))
+            .catch(error => console.log(error + "hier ist immernoch ein Fehler drin!!!!"))
     }
 
-    useEffect(() => {
-        getTodoListElemtents(urlEndpoint)
-    }, []) //wann soll ausgeführt werden: ller -> anfang oder immer wenn:  ???
+    const getInProgressListElemtents = (jsonUrl) => {
+        fetch(jsonUrl)
+            .then(response => response.json())
+            .then ( responseJson => setInProgressStatus(responseJson
+                .filter(inProgressStatus => inProgressStatus.status.includes("inProgress"))
+            ))
+            .then(data => console.log("hier die ausgabe von use Effect: " + data))
+            .catch(error => console.log(error + "hier ist immernoch ein Fehler drin!!!!"))
+    }
 
-*/
+    const getDoneListElemtents = (jsonUrl) => {
+        fetch(jsonUrl)
+            .then(response => response.json())
+            .then ( responseJson => setDoneStatus(responseJson
+                .filter(doneStatus => doneStatus.status.includes("done"))
+            ))
+            .then(data => console.log("hier die ausgabe von use Effect: " + data))
+            .catch(error => console.log(error + "hier ist immernoch ein Fehler drin!!!!"))
+    }
+
+        useEffect(() => {
+            getTodoListElemtents(urlEndpoint)
+        }, []) //wann soll ausgeführt werden: ller -> anfang oder immer wenn:  ???
+
+        useEffect(() => {
+            getInProgressListElemtents(urlEndpoint)
+        }, []) //wann soll ausgeführt werden: ller -> anfang oder immer wenn:  ???
+
+         useEffect(() => {
+            getDoneListElemtents(urlEndpoint)
+        }, []) //wann soll ausgeführt werden: ller -> anfang oder immer wenn:  ???
+
+
 
 
     return (
         <div>
 
-                <TodoList todos={schnitzel}/>
-
+            <TodoList todos={todoStatus}/>
+            <TodoList todos={inProgressStatus}/>
+            <TodoList todos={doneStatus}/>
 
             <div className="control-elements">
-                <button>trigger GET all</button>
-                <button>BUTTON</button>
+                <button>todo erstellen</button>
                 <input value="String" />
             </div>
         </div>
